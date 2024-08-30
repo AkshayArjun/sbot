9a7682328f5793ff0852faf0777e6c2fd9025023
AkshayArjun <aakshay1114@gmail.com>
2024-08-30 17:56:22 +0530

    fixed grav

--- sbot/sbot_bullet.py
+++ sbot/sbot_bullet.py
@@ -4,12 +4,14 @@ import pybullet_data
 # Initialize the simulation
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0, 0, -9.81)
+
 planeId = p.loadURDF("plane.urdf")
 snakeId = p.loadURDF("/home/akshay/snake_ws/src/sbot/sbot/snake.urdf" , [0, 0, 0] , p.getQuaternionFromEuler([0, 0, 0]))
 
-
+p.setGravity(0, 0, -10)
+p.setRealTimeSimulation(1)
 # Simulation loop
+
 while True:
     # Step the simulation
     p.stepSimulation()
