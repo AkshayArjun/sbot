7d64cefd1696a890284a5354c2a1b9ea12eac707
AkshayArjun <aakshay1114@gmail.com>
2024-09-02 16:40:04 +0530

    trial

--- sbot/sbot_bullet.py
+++ sbot/sbot_bullet.py
@@ -6,16 +6,15 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 planeId = p.loadURDF("plane.urdf")
-snakeId = p.loadURDF("/home/akshay/snake_ws/src/sbot/sbot/snake.urdf" , [0, 0, 0] , p.getQuaternionFromEuler([0, 0, 0]))
+snakeId = p.loadURDF("/home/akshay/snake_ws/src/sbot/description/snake_servo.urdf" , [0, 0, 0] , p.getQuaternionFromEuler([0, 0, 0]))
 
 p.setGravity(0, 0, -10)
 p.setRealTimeSimulation(1)
 # Simulation loop
 
 while True:
-    # Step the simulation
+    # Step the simulation 
     p.stepSimulation()
-
     p.getCameraImage(320, 200)
 
 # Disconnect from the simulation
